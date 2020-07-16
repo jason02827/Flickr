f7833f2aef4759e502aa2a8aef2ef80b549b314b
jason02827 <jason02827@hotmail.com>
2020-07-16 15:34:55 +0800

    移除storyboard UI 由AppDelegate起始畫面

---
+++ ficker/Common/UNavigationController.swift
@@ -0,0 +1,30 @@
+//
+//  UNavigationController.swift
+//  ficker
+//
+//  Created by Chenpoting on 2020/7/15.
+//  Copyright © 2020 Chenpoting. All rights reserved.
+//
+
+import UIKit
+
+class UNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ficker/Common/UTabBarController.swift
@@ -0,0 +1,24 @@
+//
+//  UTabBarController.swift
+//  ficker
+//
+//  Created by Chenpoting on 2020/7/15.
+//  Copyright © 2020 Chenpoting. All rights reserved.
+//
+
+import UIKit
+
+class UTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let vc1 = SearchViewController()
+        let vc2 = FavoriteViewController()
+        vc1.tabBarItem = UITabBarItem(tabBarSystemItem: UITabBarItem.SystemItem.featured, tag: 1)
+        vc2.tabBarItem = UITabBarItem(tabBarSystemItem: UITabBarItem.SystemItem.favorites, tag: 2)
+        viewControllers = [UNavigationController(rootViewController: vc1),
+                           UNavigationController(rootViewController: vc2)]
+    }
+    
+
+}
